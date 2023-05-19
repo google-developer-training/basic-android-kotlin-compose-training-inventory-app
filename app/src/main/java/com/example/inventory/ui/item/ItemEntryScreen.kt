7f5faf00d192fbd7054e3be8bf2fac0c775433fe
Android dev <androidedu@google.com>
2023-05-19 11:16:09 -0700

    Add scroll behavior

--- app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
@@ -20,7 +20,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -81,6 +83,7 @@ fun ItemEntryScreen(
             },
             modifier = Modifier
                 .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
     }
