14b8b8c6380a700908112b849554dc63e5f368b1
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-01 14:37:34 +0900

    Update gradle. - "room_version" = "2.6.1" - org.jetbrains.kotlin.android = 1.9.22 - rootProject.layout.buildDirectory

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("room_version", "2.5.2")
+        set("room_version", "2.6.1")
     }
 }
 
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.android.library") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
