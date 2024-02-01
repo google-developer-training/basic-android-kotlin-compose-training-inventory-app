0cc7690e1737047faccd2d82372f089cb99dc0e9
Jaehwa Noh <shwoghk14@gmail.com>
2024-02-01 15:48:37 +0900

    Update gradle. - compose-bom = 2024.01.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
 
 dependencies {
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.01.00"))
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
