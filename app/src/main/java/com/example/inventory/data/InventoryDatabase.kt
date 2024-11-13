9b96f6cfb6fba7bc33620c406e8461454728626d
Theresia Artha Putri <therenoona@gmail.com>
2024-11-13 13:25:47 +0700

    Add comment at InventoryDatabase

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -22,25 +22,41 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 /**
- * Database class with a singleton Instance object.
+ * Database Room berfungsi untuk merepresentasikan lokal di aplikasi.
+ * Pada Inventory Database disediakan akses ke database dengan menggunakan DAO.
+ * Melalui database hanya akan ada satu instance dari database yang digunakan
+ * untuk menghemat resources.
  */
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class InventoryDatabase : RoomDatabase() {
-
+    /**
+     * Fungsi abstrak ini, mengembalikan instance dari ItemDao.
+     * DAO ini digunakan untuk mengakses dan mengelola data `Item` di dalam database.
+     * Semua operasi database, seperti menambah, memperbarui, menghapus, atau mengambil data `Item`,
+     * dilakukan melalui ItemDao.
+     */
     abstract fun itemDao(): ItemDao
 
     companion object {
+        // Variabel `Instance` bersifat volatile untuk memastikan sinkronisasi antar-thread.
         @Volatile
         private var Instance: InventoryDatabase? = null
 
+        /**
+         * Ini adalah fungsi getDatabase menyediakan instance InventoryDatabase yang bisa digunakan oleh komponen
+         * aplikasi lain. Jika `Instance` sudah ada, maka fungsi akan mengembalikan instance tersebut.
+         * Jika belum, fungsi akan membuat instance baru menggunakan `Room.databaseBuilder`.
+         * context, digunakan untuk membangun atau mengakses database.
+         * @return Instance dari InventoryDatabase yang akan digunakan oleh seluruh aplikasi.
+         */
         fun getDatabase(context: Context): InventoryDatabase {
             // if the Instance is not null, return it, otherwise create a new database instance.
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
                     /**
-                     * Setting this option in your app's database builder means that Room
-                     * permanently deletes all data from the tables in your database when it
-                     * attempts to perform a migration with no defined migration path.
+                     * Metode fallbackToDestructiveMigration digunakan saat migrasi belum didefinisikan.
+                     * Room akan menghapus seluruh data yang ada agar dapat menginisialisasi ulang database
+                     * dengan struktur terbaru, menghindari kesalahan migrasi.
                      */
                     .fallbackToDestructiveMigration()
                     .build()
