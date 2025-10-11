94de7df958a10aa84f05c524a9078940aced6e91
Vaceslav676 <vaceslavzubenko676@gmail.com>
2025-10-11 15:47:02 +0300

    Реализовал пункт 7. Удаление через подтверждение

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.inventory.ui.item
 
 import androidx.lifecycle.SavedStateHandle
@@ -23,6 +7,7 @@ import com.example.inventory.data.ItemsRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class ItemDetailsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -42,6 +27,15 @@ class ItemDetailsViewModel(
             started = SharingStarted.WhileSubscribed(5_000L),
             initialValue = ItemDetailsUiState()
         )
+
+    fun deleteItem() {
+        val item = uiState.value.itemDetails.toItem()
+        if (item.id != 0) {
+            viewModelScope.launch {
+                itemsRepository.deleteItem(item)
+            }
+        }
+    }
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
