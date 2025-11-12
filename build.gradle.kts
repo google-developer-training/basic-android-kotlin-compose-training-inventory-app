59570f98cd3052de3eec291979653396ffce1b0b
hmsmurtaza <s.murtaza1@gmail.com>
2025-11-12 23:21:17 +0500

    Created Room database, Dao and Entity class

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("room_version", "2.6.0")
+        set("room_version", "2.8.3")
     }
 }
-
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("com.android.library") version "8.13.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
